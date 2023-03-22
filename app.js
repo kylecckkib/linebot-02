85708a86548ca71d7fff402a0f877d0ad807690f
kyle.chang <kyle3600@gmail.com>
2023-03-22 14:21:12 +0800

    新增註解

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+//環境設定讓開發者可以在自己擅長的環境驗測程式
 require('dotenv').config();
 
+// import 開發工具SDK / Module
 const express = require('express');
 const line = require('@line/bot-sdk');
 const { Configuration, OpenAIApi } = require("openai");
-// create LINE SDK config from env variables
+
+// 設定line sdk的環境變數
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
@@ -28,33 +31,37 @@ app.post('/callback', line.middleware(config), (req, res) => {
     });
 });
 
+// 設定openai api的key
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+// 建立openai  物件
 const openai = new OpenAIApi(configuration);
-// event handler
 
+// 訊息(event)處理
 async function handleEvent(event) {
+  
+  //如果訊息類席不是message或是非文字，回值null不進行回應
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
-
+  // 宣告使用openai的module來回答，此為chatgpt 3.0
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: event.message.text ,
-	max_tokens: 1000,
+    max_tokens: 1000,
   });
 
-  // create a echoing text message
+  // 建立回傳訊息的格式
   const echo = { type: 'text', text: completion.data.choices[0].text.trim() };
 
-  // use reply API
+  // 回傳訊息，use reply API
   return client.replyMessage(event.replyToken, echo);
 }
 
-// listen on port
+// 應用程式的監聽port: listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
